1c26c3ff1bdae44fc2f5a00267861c8ab46752df
Michael Montero <monterom334@gmail.com>
2017-09-10 15:15:45 -0700

    Add loading xml and new Interface

---
+++ app/src/main/java/developer/montero/michael/com/popularmovies/interfaces/LoadMoreListener.java
@@ -0,0 +1,9 @@
+package developer.montero.michael.com.popularmovies.interfaces;
+
+/**
+ * Created by Michael A. Montero on 10/09/2017.
+ */
+
+public interface LoadMoreListener {
+    void onLoadMoreListener();
+}
